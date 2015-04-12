7ae742de0a0594532688e68f09f78e8e55becc32
waeswaes <a.waeswaes@gmail.com>
2015-04-12 15:50:42 -0300

    implementado la recomendacion de developers dado un issue

--- src/main/java/org/tesys/recomendations/DevelopersCriteriaIssues.java
+++ src/main/java/org/tesys/recomendations/DevelopersCriteriaIssues.java
@@ -1,15 +1,43 @@
 package org.tesys.recomendations;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.tesys.core.estructures.Developer;
 import org.tesys.core.estructures.Issue;
 import org.tesys.core.estructures.Metric;
+import org.tesys.core.project.tracking.IssuePOJO;
 
 public class DevelopersCriteriaIssues {
 	
-	public List<DeveloperWithOneAcumMetric> getBestDeveloperIssue(Metric criteria, List<Issue> similarIssues) {
-		List<DeveloperWithOneAcumMetric> developers = new LinkedList<DeveloperWithOneAcumMetric>();
+	public List<RecomendedDeveloper> getBestDeveloperIssue(Metric criteria, List<Developer> similarIssues, IssuePOJO ip) {
+		List<RecomendedDeveloper> developers = new LinkedList<RecomendedDeveloper>();
+		List<String> originalLabels = new ArrayList<String>(Arrays.asList(ip.getLabels()));
+		
+		for (Developer d : similarIssues) {
+			Double similarity = 0.0;
+			
+			Double metric = null;
+			
+			for (Issue i : d.getIssues()) {
+				List<String> s = new ArrayList<String>(Arrays.asList(i.getLabels()));
+				s.retainAll(originalLabels);
+				if( similarity < s.size() ) {
+					similarity = (double)s.size();
+				}
+				
+				if(metric==null) {
+					metric = criteria.evaluate(i);
+				} else {
+					metric = criteria.getType().avg(metric, criteria.evaluate(i));
+				}
+				
+			}
+			
+			developers.add( new RecomendedDeveloper(d.getName(), metric, (double)d.getIssues().size(), similarity) );
+		}
 		
 		
 		
