30ced0c39c1c77e280c27eadcba2b3222be5ee8c
Leandro <leandro.c.lezcano@gmail.com>
2015-04-25 14:06:01 -0300

    Agregado de funcionalidad (removeChart) y delegado de responsabilidades (construir el AmChart)

--- src/main/webapp/js/charts/barPlotter.js
+++ src/main/webapp/js/charts/barPlotter.js
@@ -1,13 +1,20 @@
-define(["adaptor"], function (adaptor) {
-/**
-    Actualiza un Chart automaticamente.
-*/
-  // var chartAdapt = require('charts/chartAdaptor') ;
+define(['adaptor', 'amcharts.serial'], function (adaptor, AmCharts) {
 
-  //amChart: amChart
-  //keys: string[]
-  function BarChartPlotter(amChart, keys) {
-    this.chart = amChart ;
+  /**
+   * [BarChartPlotter Similar al RadarChartPlotter, solo que no normaliza los
+   * datos, osea que los valores de los vectores en cada grafico son dibujados
+   * con los valores originales.
+   * La descripcion de los parametros y metodos son identicos que los de
+   * RadarChartPlotter]
+   * 
+   * @param {[type]} amChartHTMLContainer [description]
+   * @param {[type]} amChartParams        [description]
+   * @param {[type]} keys                 [description]
+   */
+  function BarChartPlotter(amChartHTMLContainer, amChartParams, keys) {
+    this.chart = AmCharts.makeChart(amChartHTMLContainer, amChartParams);
+    this.amChartParams = amChartParams;
+    this.amChartHTMLContainer = amChartHTMLContainer;
     this.adaptor = new adaptor() ;
     this.keys = keys ;
     this.dataProvider = this.adaptor.generateDataProvider(this.keys);
@@ -29,8 +36,19 @@ define(["adaptor"], function (adaptor) {
     this.chart.animateAgain();
   };
 
-  BarChartPlotter.prototype.build = function (amChart, keys) {
-    this.chart = amChart ;
+  BarChartPlotter.prototype.removeGraph = function (tag) {
+    for (var i = 0; i<this.chart.graphs.length; i++){
+      if (this.chart.graphs[i].valueField==tag){
+        this.chart.removeGraph(this.chart.graphs[i]);
+      }
+    }
+  };
+
+  BarChartPlotter.prototype.build = function (keys) {
+    this.chart = AmCharts.makeChart(
+      this.amChartHTMLContainer, 
+      this.amChartParams
+    );
     this.adaptor = new adaptor() ;
     this.keys = keys ;
     this.dataProvider = this.adaptor.generateDataProvider(this.keys);    
