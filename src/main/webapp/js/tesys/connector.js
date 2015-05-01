918cf27bd78b92630eb3846597bf109d5a61b898
Leandro <leandro.c.lezcano@gmail.com>
2015-05-01 20:35:31 -0300

    Se movieron los metodos 'score', 'joinMetrics' y 'storeAnalysis' del modulo 'extractor' al 'connector'

--- src/main/webapp/js/tesys/connector.js
+++ src/main/webapp/js/tesys/connector.js
@@ -2,7 +2,7 @@ define(["jquery"], function($) {
   function getAnalysis(callback) {
     $.ajax({
       type: 'GET',
-      url: location+'rest/controller/developers/0',
+      url: "file:////home/leandro/Tesis/workspace/tesys/src/main/webapp/developers-issues.json",
       dataType: 'json', // data type of response
       success: function(data) {
         callback(data) ;
@@ -13,7 +13,7 @@ define(["jquery"], function($) {
   function getMetrics(callback) {
     $.ajax({
         type: 'GET',
-        url: location+'rest/controller/metrics',
+        url: "file:////home/leandro/Tesis/workspace/tesys/src/main/webapp/metrics.json",
         dataType: "json", // data type of response
         success: function (data) {
           callback(data) ;
@@ -24,7 +24,7 @@ define(["jquery"], function($) {
   function getSkills(callback) {
     $.ajax({
       type: 'GET',
-      url: location+'rest/controller/skills',
+      url: "file:////home/leandro/Tesis/workspace/tesys/src/main/webapp/skills.json",
       dataType: "json", 
       success: function (data) {
         callback(data);
@@ -32,9 +32,66 @@ define(["jquery"], function($) {
     });
   }
 
+  function score(puntuador, puntuado, issue, puntuacion) { 
+    var toSend = "{\"puntuador\": \"" + puntuador + "\"," +
+              "\"puntuado\": \"" + puntuado + "\"," +
+              "\"issue\": \"" + issue + "\"," +
+              "\"puntuacion\": \"" + puntuacion + "\"" +"}";
+
+    $.ajax({
+        url: location+'rest/controller/punt',
+        type: 'PUT',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: toSend,
+        success: function (data) {
+            //TODO ver casos de error
+          alert("Puntuation stored successful");
+        }
+    });
+
+  }
+
+  function joinMetrics() {
+    $.ajax({
+        url: location+'rest/controller/analyzer',
+        type: 'PUT',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            //TODO ver casos de error
+          alert("Tesys analysis successful");
+        }
+    });   
+  }
+
+  function storeAnalysis(sonarUrl, sonarUser, sonarPass, repository, revision, sonarKey) {
+      var toSend = "{\"url\": \"" + sonarUrl + "\"," +
+          "\"user\": \"" + sonarUser + "\"," +
+          "\"pass\": \"" + sonarPass + "\"," +
+          "\"repository\": \"" + repository + "\"," +
+          "\"revision\": \"" + revision + "\"," +
+          "\"sonarKey\": \"" + sonarKey + "\"" +"}";
+      $.ajax({
+          url: location+'rest/controller/sonar',
+          type: 'PUT',
+          dataType: 'json',
+          contentType: "application/json; charset=utf-8",
+          data: toSend,
+          success: function (data) {
+              //TODO ver casos de error
+            alert("analysis stored");
+          }
+      });
+  }
+
+
   return {    
     'getAnalysis': getAnalysis,
     'getMetrics': getMetrics,
-    'getSkills': getSkills
+    'getSkills': getSkills,
+    'score': score,
+    'joinMetrics': joinMetrics,
+    'storeAnalysis': storeAnalysis
   };
 });
