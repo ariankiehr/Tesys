80068ca0cdb08e33720dab02b9db39f481012234
waeswaes <a.waeswaes@gmail.com>
2015-06-28 19:01:47 -0300

    eliminado todo lo que generaba usuarios, preparando para el merge

--- src/main/java/org/tesys/correlations/Predictions.java
+++ src/main/java/org/tesys/correlations/Predictions.java
@@ -7,30 +7,12 @@ import java.util.List;
 import java.util.Set;
 
 import org.tesys.core.analysis.skilltraceability.Skill;
-import org.tesys.core.db.AnalysisVersionsQuery;
-import org.tesys.core.db.ElasticsearchDao;
 import org.tesys.core.db.IssuesWithMetrics;
-import org.tesys.core.estructures.Developer;
 import org.tesys.core.estructures.Issue;
 
 
 
 public class Predictions {
-	
-	public static void main(String[] args) {
-		
-		//ElasticsearchDao<Issue> dao1 = new ElasticsearchDao<Issue>(Issue.class, ElasticsearchDao.DEFAULT_RESOURCE_SPRINT1);
-		ElasticsearchDao<Issue> dao2 = new ElasticsearchDao<Issue>(Issue.class, ElasticsearchDao.DEFAULT_RESOURCE_SPRINT2);
-		
-		List<Issue> l = Jarco.getIssues();
-		
-		for (Issue issue : l) {
-			//dao1.create(issue.getIssueId(), issue);
-			dao2.create(issue.getIssueId(), issue);
-		}
-		
-		
-	}
 
 	public static List<DeveloperPrediction> getPredictions(String metricKey,
 			Double value, Double correlationVariation, int sprint, List<String> skills) {
@@ -93,8 +75,6 @@ public class Predictions {
 							List<Double> lr = LinearRegression.getRegression(pearson1, pearson2);
 							metricPrediction.putMetric(metrics.get(j), lr.get(0)*value+lr.get(1));
 							metricPrediction.putDeviation(metrics.get(j), lr.get(2));
-							//MetricPrediction mp = new MetricPrediction( metrics.get(j), lr.get(0)*value+lr.get(1), lr.get(2));
-							//metricPrediction.add(mp);
 						}
 						
 					}
@@ -108,67 +88,5 @@ public class Predictions {
 		return developerPrediction;
 		
 	}
-	
-	
-	
-	public static List<MetricPrediction> getPredictionsMean( String metricKey, 
-			Double value, Double correlationVariation) {
-		
-		List<MetricPrediction> metricPrediction = new ArrayList<MetricPrediction>();
-		
-		IssuesWithMetrics is = new IssuesWithMetrics(0);
-		List<Issue> l = is.execute();
-
-		
-		List<Double> pearson1 = new ArrayList<Double>();
-		List<Double> pearson2 = new ArrayList<Double>();
-		List<String> metrics = new ArrayList<String>();
-		
-		
-		metrics.addAll(l.get(0).getMetrics().keySet());
-		metrics.remove("quacode");
-		metrics.remove("prec");
-		
-		for (int i = 0; i < metrics.size(); i++) {
-			for (int j = 0; j < metrics.size(); j++) {
-				if( i != j ) {
-					
-					for (Issue issue : l) {
-						if( metrics.get(i).equals(metricKey) ) { 
-							List<String> isk = new LinkedList<String>();
-							for (Skill sk : issue.getSkills()) {
-								isk.add(sk.skillName);
-							}
-							
-							pearson1.add(issue.getMetrics().get(metrics.get(i)));
-							pearson2.add(issue.getMetrics().get(metrics.get(j)));
-
-						}
-
-					}
-					
-					Double dou = Pearson.getCorrelation(pearson1, pearson2);
-					
-					if (dou>correlationVariation || dou < -correlationVariation) {
-						List<Double> lr = LinearRegression.getRegression(pearson1, pearson2);
-						MetricPrediction mp = new MetricPrediction(metrics.get(j), lr.get(0)*value+lr.get(1), lr.get(2));
-						metricPrediction.add(mp);
-					}
-					
-
-					pearson1.clear();
-					pearson2.clear();
-					
-				}
-
-			}
-		}
-		
-		
-		return metricPrediction;
-		
-	}
-	
-	
 	
 }
