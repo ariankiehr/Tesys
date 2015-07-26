8010177d126fcf12ef710021c4e5514f8497261a
Leandro <leandro.c.lezcano@gmail.com>
2015-07-26 17:15:19 -0300

    Mejoras al conector. Logueo de errores en el parseo de responses. Uso de location.origin

--- src/main/webapp/js/tesys/connector.js
+++ src/main/webapp/js/tesys/connector.js
@@ -1,9 +1,16 @@
 define(["jquery"], function($) {
+
+  var apiPath = '/tesys/rest/controller/';
+
   function getAnalysis(callback) {
     $.ajax({
       type: 'GET',
-      url: location+'rest/controller/developers/0',
+      url: location.origin+apiPath+'developers/0',
       dataType: 'json', // data type of response
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error en el response de "+apiPath+"developers/0");
+        console.log(jqXHR, textStatus, errorThrown);
+      },
       success: function(data) {
         callback(data) ;
       }
@@ -12,20 +19,28 @@ define(["jquery"], function($) {
 
   function getMetrics(callback) {
     $.ajax({
-        type: 'GET',
-        url: location+'rest/controller/metricsavailable',
-        dataType: "json", // data type of response
-        success: function (data) {
-          callback(data) ;
-        }
+      type: 'GET',
+      url: location.origin+apiPath+'metricsavailable',
+      dataType: "json", // data type of response
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error en el response de "+apiPath+"metricsavailable");
+        console.log(jqXHR, textStatus, errorThrown);
+      },
+      success: function (data) {
+        callback(data) ;
+      }
     });
   }
 
   function getSkills(callback) {
     $.ajax({
       type: 'GET',
-      url: location+'rest/controller/skills',
+      url: location.origin+apiPath+'skills',
       dataType: "json", 
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error en el response de "+apiPath+"skills");
+        console.log(jqXHR, textStatus, errorThrown);
+      },
       success: function (data) {
         callback(data);
       }
@@ -57,8 +72,12 @@ define(["jquery"], function($) {
     });
     $.ajax({
       type: 'GET',
-      url: location+'rest/controller/getpredic/'+metricName+'/'+metricValue+'/'+pearsonFactor+'/'+sprint+skillQuery,
+      url: location.origin+apiPath+'getpredic/'+metricName+'/'+metricValue+'/'+pearsonFactor+'/'+sprint+skillQuery,
       dataType: 'json', // data type of response
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error en el response de "+apiPath+"getpredic");
+        console.log(jqXHR, textStatus, errorThrown);
+      },
       success: function(data) {
         callback(data) ;
       }
@@ -74,11 +93,15 @@ define(["jquery"], function($) {
               "\"puntuacion\": \"" + puntuacion + "\"" +"}";
 
     $.ajax({
-        url: location+'rest/controller/punt',
+        url: location.origin+apiPath+'punt',
         type: 'PUT',
         dataType: 'json',
         contentType: "application/json; charset=utf-8",
         data: toSend,
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert("Error en el response de "+apiPath+"punt");
+          console.log(jqXHR, textStatus, errorThrown);
+        },
         success: function (data) {
             //TODO ver casos de error
           alert("Puntuation stored successful");
@@ -89,10 +112,14 @@ define(["jquery"], function($) {
 
   function joinMetrics() {
     $.ajax({
-        url: location+'rest/controller/analyzer',
+        url: location.origin+apiPath+'analyzer',
         type: 'PUT',
         dataType: 'json',
         contentType: "application/json; charset=utf-8",
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert("Error en el response de "+apiPath+"analyzer");
+          console.log(jqXHR, textStatus, errorThrown);
+        },
         success: function (data) {
             //TODO ver casos de error
           alert("Tesys analysis successful");
@@ -101,23 +128,27 @@ define(["jquery"], function($) {
   }
 
   function storeAnalysis(sonarUrl, sonarUser, sonarPass, repository, revision, sonarKey) {
-      var toSend = "{\"url\": \"" + sonarUrl + "\"," +
-          "\"user\": \"" + sonarUser + "\"," +
-          "\"pass\": \"" + sonarPass + "\"," +
-          "\"repository\": \"" + repository + "\"," +
-          "\"revision\": \"" + revision + "\"," +
-          "\"sonarKey\": \"" + sonarKey + "\"" +"}";
-      $.ajax({
-          url: location+'rest/controller/sonar',
-          type: 'PUT',
-          dataType: 'json',
-          contentType: "application/json; charset=utf-8",
-          data: toSend,
-          success: function (data) {
-              //TODO ver casos de error
-            alert("analysis stored");
-          }
-      });
+    var toSend = "{\"url\": \"" + sonarUrl + "\"," +
+      "\"user\": \"" + sonarUser + "\"," +
+      "\"pass\": \"" + sonarPass + "\"," +
+      "\"repository\": \"" + repository + "\"," +
+      "\"revision\": \"" + revision + "\"," +
+      "\"sonarKey\": \"" + sonarKey + "\"" +"}";
+    $.ajax({
+      url: location.origin+apiPath+'sonar',
+      type: 'PUT',
+      dataType: 'json',
+      contentType: "application/json; charset=utf-8",
+      data: toSend,
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error en el response de "+apiPath+"sonar");
+        console.log(jqXHR, textStatus, errorThrown);
+      },
+      success: function (data) {
+          //TODO ver casos de error
+        alert("analysis stored");
+      }
+    });
   }
 
   /**
@@ -130,33 +161,37 @@ define(["jquery"], function($) {
    *     metrica al servidor la misma podra ser usada para mostrar en la UI la respuesta del servidor.
    */
   function storeMetric(metricName, metricDescription, metricFunction, callback){
-      //TODO hacer un md5 a metricName para obtener metricId
-      var tosend = "{\"key\": \"" + metricName + "\"," +
-              "\"nombre\": \"" + metricName + "\"," +
-              "\"descripcion\": \"" + metricDescription + "\"," +
-              "\"procedencia\": \"" + "tesys" + "\"," +
-               metricFunction + "}";  
-      $.ajax({
-          url: location+'rest/controller/newmetric',
-          type: 'POST',
-          dataType: 'json',
-          contentType: "application/json; charset=utf-8",
-          success: function (data) {
-            callback(data);
-          },
-          data: tosend
-        });          
+    //TODO hacer un md5 a metricName para obtener metricId
+    var tosend = "{\"key\": \"" + metricName + "\"," +
+      "\"nombre\": \"" + metricName + "\"," +
+      "\"descripcion\": \"" + metricDescription + "\"," +
+      "\"procedencia\": \"" + "tesys" + "\"," +
+       metricFunction + "}";  
+    $.ajax({
+      url: location.origin+apiPath+'newmetric',
+      type: 'POST',
+      dataType: 'json',
+      contentType: "application/json; charset=utf-8",
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error en el response de "+apiPath+"newmetric");
+        console.log(jqXHR, textStatus, errorThrown);
+      }, 
+     success: function (data) {
+        callback(data);
+      },
+      data: tosend
+    });          
   }
 
 
   return {    
-    'getAnalysis': getAnalysis,
-    'getMetrics': getMetrics,
-    'getSkills': getSkills,
-    'score': score,
-    'joinMetrics': joinMetrics,
-    'storeAnalysis': storeAnalysis,
-    'storeMetric': storeMetric,
+    getAnalysis: getAnalysis,
+    getMetrics: getMetrics,
+    getSkills: getSkills,
+    score: score,
+    joinMetrics: joinMetrics,
+    storeAnalysis: storeAnalysis,
+    storeMetric: storeMetric,
     getPredictions: getPredictions
   };
 });
